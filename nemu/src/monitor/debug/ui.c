0024edaec9245b34c7d57c6a578842c929fa6961
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-09 12:13:53 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:13:53 up  1:57,  1 user,  load average: 0.05, 0.01, 0.00 22ef2173540e0ccdbc7031a6b2c52e9e4fbfe45

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,27 +37,28 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args) {
-	int step_count = 1;  // 默认执行1条指令
-	
-	if (args != NULL) {
-		// 解析参数
+	int step_count = 1;
+
+	if (args != NULL){
 		char *arg = strtok(args, " ");
-		if (arg != NULL) {
-			// 尝试将参数转换为整数
+		if (arg != NULL){
 			char *endptr;
 			long parsed_count = strtol(arg, &endptr, 10);
-			
-			// 检查转换是否成功
-			if (*endptr == '\0' && parsed_count > 0) {
+			if (*endptr == '\0' && parsed_count > 0){
 				step_count = (int)parsed_count;
-			} else {
-				printf("Invalid step count: %s. Using default value 1.\n", arg);
+			}
+			else{
+				printf("Invalid step count\n");
 			}
 		}
 	}
-	
-	printf("Executing %d instruction(s)...\n", step_count);
-	cpu_exec(step_count);
+	if (step_count == 1){
+		cpu_exec(1);
+	}
+	else if (step_count > 1){
+		printf("Executing %d instruction(s)...\n", step_count);
+		cpu_exec(step_count);
+	}
 	return 0;
 }
 
@@ -70,7 +71,7 @@ static struct {
 } cmd_table [] = {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
-	{ "si", "Single step execution (si [count])", cmd_si },
+	{ "si", "Single step execution (si [count])", cmd_si},
 	{ "q", "Exit NEMU", cmd_q },
 
 	/* TODO: Add more commands */
