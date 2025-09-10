9a6b08c0c2a78353f6422870544641075f6feda3
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 17:41:46 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:41:46 up  5:15,  1 user,  load average: 0.08, 0.14, 0.05 a4c3800195ceffd2fb6bc61d89abe9c99126bf22

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -228,6 +228,8 @@ uint32_t eval(int p, int q){
 			case '-': return val1 - val2;
 			case '*': return val1 * val2;
 			case '/': return val1 / val2;
+			case EQ: return val1 == val2;
+			case NEQ: return val1 != val2;
 			default: assert(0);
 		}
 	}
