44a42c56662bf264ce0e15419a9c9c6338424e71
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 19:11:17 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:11:17 up  6:23,  1 user,  load average: 0.27, 0.22, 0.19 942e0b6f5688a0058338bb1cf93ffce93e01ebdf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -120,6 +120,7 @@ static bool make_token(char *e) {
 						strncpy(tokens[nr_token].str, substr_start + 1, substr_len - 1);
 						tokens[nr_token].str[substr_len - 1] = '\0';
 						nr_token++;
+						break;
 					default: panic("please implement me");
 				}
 				break;
