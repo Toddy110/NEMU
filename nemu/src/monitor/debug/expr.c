223d50294905dec9eff84313e513112948d15d7c
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 15:21:48 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:21:48 up  3:50,  1 user,  load average: 0.00, 0.02, 0.00 7620880883bf2a132f222856c8a4b01532968dbe

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -16,21 +16,22 @@ enum {
 static struct rule {
 	char *regex;
 	int token_type;
+	int priority;
 } rules[] = {
 
 	/* TODO: Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
 
-	{" +",	NOTYPE},				// spaces
-	{"\\+", '+'},					// plus
-	{"==", EQ},						// equal
-	{"\\b[0-9]+\\b", NUMBER},	    // decimal number
-	{"\\(", '('},					// left parenthesis
-	{"\\)", ')'},					// right parenthesis
-	{"-", '-'},						// minus
-	{"\\*", '*'},					//multiply
-	{"/", '/'}						//divide
+	{" +",	NOTYPE, 0},					// spaces
+	{"\\+", '+', 2},					// plus
+	{"==", EQ, 1},						// equal
+	{"\\b[0-9]+\\b", NUMBER, 0},	    // decimal number
+	{"\\(", '(', 4},					// left parenthesis
+	{"\\)", ')', 4},					// right parenthesis
+	{"-", '-', 2},						// minus
+	{"\\*", '*', 3},					//multiply
+	{"/", '/', 3}						//divide
 	
 };
 
@@ -58,6 +59,7 @@ void init_regex() {
 typedef struct token {
 	int type;
 	char str[32];
+	int priority;
 } Token;
 
 Token tokens[32];
@@ -96,6 +98,7 @@ static bool make_token(char *e) {
 					case ')':
 					case EQ:
 						tokens[nr_token].type = rules[i].token_type;
+						tokens[nr_token].priority = rules[i].priority;
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
 						nr_token++;
@@ -114,6 +117,82 @@ static bool make_token(char *e) {
 	}
 
 	return true; 
+}
+
+int dominant_operator(int p, int q){
+	int min_priority = 20;
+	int op_pos = -1;
+	int parentheses = 0;
+	int i;
+	for (i = p; i <= q; i++){
+		if (tokens[i].type == '('){
+			parentheses++;
+		}
+		else if (tokens[i].type == ')'){
+			parentheses--;
+		}
+		else if (parentheses == 0){
+			if (tokens[i].priority <= min_priority){
+				min_priority = tokens[i].priority;
+				op_pos = i;
+			}
+		}
+	}
+	assert(op_pos != -1);
+	return op_pos;
+}
+
+bool check_parentheses(int p, int q){
+	if (tokens[p].type != '(' || tokens[q].type != ')')
+		return false;
+	int count = 0;
+	int pos;
+	for (pos = p + 1; pos <= q - 1; pos++){
+		if (tokens[pos].type == '('){
+			count++;
+		}
+		else if (tokens[pos].type == ')'){
+			count--;
+		}
+		if (count < 0){
+			return false;
+		}
+	}
+	if (count){
+		return false;
+	}
+	return true;
+}
+
+uint32_t eval(int p, int q){
+	if (p > q) {
+		assert(0);
+	}
+	else if (p == q){
+		uint32_t value = 0;
+		if (tokens[p].type == NUMBER){
+			sscanf(tokens[p].str, "%d", &value);
+		}
+	}
+	else if (check_parentheses(p, q) == true){
+		return eval(p + 1, q - 1);
+	}
+
+	else{
+		int op = dominant_operator(p, q);
+		uint32_t val1 = eval(p,op - 1);
+		uint32_t val2 = eval(op + 1, q);
+
+		switch (tokens[op].type){
+			case '+': return val1 + val2;
+			case '-': return val1 - val2;
+			case '*': return val1 * val2;
+			case '/': return val1 / val2;
+			default: assert(0);
+		}
+	}
+	assert (1);
+	return 0;
 }
 
 uint32_t expr(char *e, bool *success) {
