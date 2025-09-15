9cff78914f41dec9054e01e89ab6227c12b7edfc
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-15 11:45:04 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:45:04 up  2:23,  1 user,  load average: 0.22, 0.13, 0.07 7963827939530f33bfb1375a1ce4380e8ec3bac0

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,7 +1,7 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
-#define NR_WP 32
+#define NR_WP 33
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
@@ -19,5 +19,43 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(){
+	WP *f_wp, *current_wp;
+	f_wp = free_;
+	free_ = free_->next;
+	f_wp->next = NULL;
+	current_wp = head;
+	if (current_wp == NULL){
+		head = f_wp;
+		head->next = NULL;
+	} 
+	else{
+		while (current_wp->next != NULL){
+			current_wp = current_wp->next;
+		}
+		current_wp->next = f_wp;
+	}
+	return f_wp;
+}
+
+void free_wp(WP *wp){
+	WP *current_wp = head;
+	WP *prev_wp = NULL;
+	while (current_wp != NULL && current_wp != wp){
+		prev_wp = current_wp;
+		current_wp = current_wp->next;
+	}
+	if (current_wp == NULL){
+		assert(0);
+	}
+	if (prev_wp == NULL){
+		head = head->next;
+	}
+	else{
+		prev_wp->next = current_wp->next;
+	}
+	wp->next = free_;
+	free_ = wp;
+}
 
 
