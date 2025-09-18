6b0817891e69652bd6b835275b30d92b909a30d1
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 20:45:03 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:45:03 up  4:19,  1 user,  load average: 0.30, 0.16, 0.11 5ef4358dd8dc0e8f8a1d27f6efa71e6157df91a5

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -25,12 +25,12 @@ make_group(group1_b,
 /* 0x81 */
 make_group(group1_v,
 	inv, inv, inv, inv, 
-	and_i2rm_v, sub_i2rm_v, inv, cmp_si2rm_v)
+	and_i2rm_v, sub_i2rm_v, inv, inv)
 
 /* 0x83 */
 make_group(group1_sx_v,
 	inv, or_si2rm_v, inv, inv, 
-	and_si2rm_v, sub_si2rm_v, inv, inv)
+	and_si2rm_v, sub_si2rm_v, inv, cmp_si2rm_v)
 
 /* 0xc0 */
 make_group(group2_i_b,
