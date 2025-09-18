98851d65ca78a7b1a6e4303e4fab95143e326360
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 21:10:19 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:10:19 up  4:44,  1 user,  load average: 0.11, 0.20, 0.18 27ce7d4c37a46d46981028c7de581e0e22ac08db

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -3,7 +3,7 @@
 #define instr ret
 
 static void do_execute(){
-    cpu.eip = swaddr_read(cpu.eip, 4) - 1 - DATA_BYTE;
+    cpu.eip = swaddr_read(cpu.esp, 4) - 1 - DATA_BYTE;
     cpu.esp += 4;
     print_asm("ret");
 }
