36fe7e5d65bab3238c823847f611c1d5770e3765
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 20:33:13 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:33:13 up  4:07,  1 user,  load average: 0.08, 0.12, 0.10 6be4e0a8ba4246e14a10dc35d110cb4b74fe420b

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,20 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute(){
+    op_dest->val = swaddr_read(cpu.esp, 4);
+    OPERAND_W(op_dest, op_dest->val);
+    cpu.esp += 4;
+    print_asm_template1();
+}
+
+make_helper(concat(pop_r_, SUFFIX)) {
+    int reg_code = ops_decoded.opcode & 0x7;
+    op_dest->type = OP_TYPE_REG;
+    op_dest->reg = reg_code;
+    do_execute();
+    return 1;
+}
+
+#include "cpu/exec/template-end.h"
