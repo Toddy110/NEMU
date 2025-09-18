d8d03bd95009c30291931385ef3a1daf64ec3ab2
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 21:34:12 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:34:12 up  5:08,  1 user,  load average: 0.32, 0.22, 0.19 31afdfd473ce51a59a43477c346acf8bbe3deb46

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,19 +2,17 @@
 
 #define instr call
 
-static void do_execute() {
-    int instr_len = 1 + DATA_BYTE;
-    swaddr_t return_addr = cpu.eip + instr_len;
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, return_addr);
-    cpu.eip += (DATA_TYPE_S)op_src->val - instr_len;
-    print_asm("call 0x%x", return_addr + (DATA_TYPE_S)op_src->val);
-}
-
-make_helper(concat(call_i_, SUFFIX)) {
+make_helper(concat(call_i_, SUFFIX)){
     int len = concat(decode_i_, SUFFIX)(eip + 1);
-    do_execute();
+    
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.eip + len + 1);
+    
+    cpu.eip += (DATA_TYPE_S)op_src->val;
+    print_asm("call 0x%x", cpu.eip + len + 1);
+    
     return len + 1;
 }
+
 
 #include "cpu/exec/template-end.h"
