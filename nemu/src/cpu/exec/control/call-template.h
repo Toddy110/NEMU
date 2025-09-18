2a075b057fb1d83d1365c1d99f40b867b02931e2
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 21:41:50 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:41:50 up  5:15,  1 user,  load average: 1.24, 0.95, 0.52 aa46d273628a7858130a242de11db31a9ac84324

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -6,10 +6,10 @@ make_helper(concat(call_i_, SUFFIX)){
     int len = concat(decode_i_, SUFFIX)(eip + 1);
     
     cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.eip + len + 1);
+    swaddr_write(cpu.esp, 4, eip + len);  // 保存下一条指令的地址作为返回地址
     
-    cpu.eip += (DATA_TYPE_S)op_src->val;
-    print_asm("call 0x%x", cpu.eip + len + 1);
+    cpu.eip = eip + len + (DATA_TYPE_S)op_src->val;  // 跳转地址 = 当前指令地址 + 指令长度 + 偏移量
+    print_asm("call %x", cpu.eip);
     
     return len + 1;
 }
