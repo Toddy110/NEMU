fe2a5a81583b63cedd1f681e1d021a4588bd9f2b
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-19 09:49:30 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:49:30 up 4 min,  1 user,  load average: 0.31, 0.28, 0.13 379e06ef4962005b6f59ab7ef6bcda7665777e86

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,15 +2,16 @@
 
 #define instr call
 
-
-make_helper(concat(call_i_, SUFFIX)){  //定义call指令跳转相对偏移量的操作
-    int len = concat(decode_i_, SUFFIX)(eip + 1); //decode函数中解码立即数的操作获取偏移量长度
-    reg_l(R_ESP) -= DATA_BYTE;  //esp-4操作，相当于压地址入栈的第一步，栈顶地址减4
-    MEM_W(reg_l(R_ESP), cpu.eip+len+1);  //将下一步的指令地址写入栈顶
- 
-    cpu.eip += (DATA_TYPE_S)op_src->val; //加上立即数偏移量，实现跳转操作
-    print_asm("call: 0x%x", cpu.eip + len + 1);  //打印调试信息，查看跳转的具体地址
-    return len + 1;  //返回指令长度
+make_helper(concat(call_i_, SUFFIX)) {
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+    uint32_t return_addr = cpu.eip + len + 1;
+    
+    cpu.esp -= DATA_BYTE;
+    swaddr_write(cpu.esp, DATA_BYTE, return_addr);
+    cpu.eip += (DATA_TYPE_S)op_src->val;
+    
+    print_asm("call: 0x%x", return_addr);
+    return len + 1;
 }
 
 #include "cpu/exec/template-end.h"
