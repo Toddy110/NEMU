30a4f3e70f34916a33cbab308a800ad69c0da30d
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 10:56:00 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:56:00 up  1:50,  1 user,  load average: 0.18, 0.08, 0.07 17a097e71ef2be534d88ad5aecb270173bad0998

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -2,11 +2,11 @@
 
 #define instr jbe
 
-static void do_execute(){
-    if (cpu.eflags.CF || cpu.eflags.ZF){
-        cpu.eip += op_src->val;
-    }
-    print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+static void do_execute() {
+	if (cpu.eflags.CF || cpu.eflags.ZF) {
+		cpu.eip += (DATA_TYPE_S)op_src->val;
+	}
+	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
 }
 
 make_instr_helper(i)
