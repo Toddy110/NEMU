3a2f21f78452366c62b5b666625a9ee21200678c
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 10:10:48 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:10:48 up  1:05,  1 user,  load average: 0.05, 0.08, 0.08 2e24bf9d5dd1dd9f8603a342703142c0ee00ffe8

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -7,10 +7,10 @@ void do_execute(){
     OPERAND_W(op_dest, result);
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
     cpu.eflags.CF = result < op_dest->val;
-    cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+    cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
     print_asm_template2();
 }
-make_instr_helper(i2a)
+
 make_instr_helper(i2rm)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
