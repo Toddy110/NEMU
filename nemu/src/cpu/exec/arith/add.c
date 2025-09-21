b0aee27d5bc18eb1898d595b1a0046ce2db1898c
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 17:11:45 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:11:45 up  1:18,  1 user,  load average: 0.20, 0.15, 0.11 9ed2642690739c4a9947eb52e0fc84545d3614

--- nemu/src/cpu/exec/arith/add.c
+++ nemu/src/cpu/exec/arith/add.c
@@ -11,6 +11,7 @@
 
 /* for instruction encoding overloading */
 
+make_helper_v(add_i2a)
 make_helper_v(add_i2rm)
 make_helper_v(add_si2rm)
 make_helper_v(add_r2rm)
--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -6,8 +6,10 @@ make_helper(concat(call_i_, SUFFIX)) {
     int len = concat(decode_i_, SUFFIX)(eip + 1);
     uint32_t return_addr = cpu.eip + len + 1;
     
-    cpu.esp -= DATA_BYTE;
-    swaddr_write(cpu.esp, DATA_BYTE, return_addr);
+    /* In our 32-bit environment, always push 4-byte return address
+       to keep consistency with RET which pops 4 bytes. */
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, return_addr);
     cpu.eip += (DATA_TYPE_S)op_src->val;
     
     print_asm("call: 0x%x", return_addr);
