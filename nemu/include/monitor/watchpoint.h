eb02594e110057534c07e60a4881a8f94b590ed8
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-23 12:09:10 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:09:10 up 38 min,  1 user,  load average: 0.21, 0.23, 0.13 c5a031cc9d610d3d234cd16241ee12a14861bb57

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,18 @@ typedef struct watchpoint {
 	struct watchpoint *next;
 
 	/* TODO: Add more members if necessary */
-
+	char expr[128];
+	uint32_t value;
 
 } WP;
+
+/* Watchpoint interfaces */
+void init_wp_pool(void);
+void check_watchpoints(void);
+void print_watchpoints(void);
+WP *new_wp(void);
+void free_wp(WP *wp);
+WP *create_watchpoint(char *expr_str);
+void delete_watchpoint(int no);
 
 #endif
