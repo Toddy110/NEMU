927b4843e4a804f3cca5c120ec5bf4b58f15ff47
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-24 16:05:27 +0800

    >  compile kernel 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:05:27 up 26 min,  1 user,  load average: 0.25, 0.14, 0.10 ca17432ec48037a914f7767213bba7ee521e122b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,16 +42,17 @@ uint32_t loader() {
 	for(eph = ph + elf->e_phnum; ph < eph; ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-
+			uint32_t addr = ph->p_vaddr;
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 uint
+			 ramdisk_read((void *)addr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
+			memset((void *)(addr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
