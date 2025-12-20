097ac76e74185aea9606577526749381a179bd83
tracer-NEMU2020 <tjuics2020@163.com>
2025-12-20 23:57:04 +0800

    >  compile NEMU 3024244288 toddyn Linux ToddyN 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:57:04 up  8:45,  1 user,  load average: 0.41, 0.40, 0.27 df8e7e2297537518d2ed5d3292aa00222358842

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -31,13 +31,13 @@ hwaddr_t page_translate(lnaddr_t addr) {
 
     uint32_t pde_addr = cpu.cr3 + dir * 4;
     uint32_t pde = hwaddr_read(pde_addr, 4);
-        Assert(pde & 0x1, "PDE not present: lnaddr=0x%08x cr3=0x%08x pde_addr=0x%08x pde=0x%08x dir=%u",
-            addr, cpu.cr3, pde_addr, pde, dir);
+            Assert(pde & 0x1, "PDE not present: eip=0x%08x lnaddr=0x%08x cr3=0x%08x pde_addr=0x%08x pde=0x%08x dir=%u",
+                cpu.eip, addr, cpu.cr3, pde_addr, pde, dir);
 
     uint32_t pte_addr = (pde & 0xFFFFF000) + page * 4;
     uint32_t pte = hwaddr_read(pte_addr, 4);
-        Assert(pte & 0x1, "PTE not present: lnaddr=0x%08x cr3=0x%08x pte_addr=0x%08x pte=0x%08x dir=%u page=%u",
-            addr, cpu.cr3, pte_addr, pte, dir, page);
+            Assert(pte & 0x1, "PTE not present: eip=0x%08x lnaddr=0x%08x cr3=0x%08x pte_addr=0x%08x pte=0x%08x dir=%u page=%u",
+                cpu.eip, addr, cpu.cr3, pte_addr, pte, dir, page);
 
     hwaddr = (pte & 0xFFFFF000) + offset;
     tlb_fill(addr, hwaddr);
